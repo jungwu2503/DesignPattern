86efb9b39f454205f4a11bb2972c1d1caf14292b
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2021-08-04 17:00:28 +0900

    Singleton Pattern
    생성자가 여러차례 호출되더라도 실제로 생성되는 객체는 하나,
    최초 생성 이후 호출된 생성자는 최초의 생성자가 생성한 객체를 리턴
    생성된 객체를 어디에서든지 참초할수 있게함

---
+++ ChocolateController.java
@@ -0,0 +1,14 @@
+
+public class ChocolateController {
+
+	public static void main(String[] args) {
+		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+		boiler.fill();
+		boiler.boil();
+		boiler.drain();
+		
+		ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+
+	}
+
+}
